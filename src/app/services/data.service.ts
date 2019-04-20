6d64ace29d43e45a9a0f24afe5f926eb251f5b7f
Artem Shcheglov <stiw00@bk.ru>
2019-04-20 10:15:58 +0300

    fix rxjs error

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/Operators';
 import { IEvent } from '../components/ievent';
 
 @Injectable({
