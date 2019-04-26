13f60493b3c393b2bcde5272cffa6c3bf3f377ea
ArtemSglv <stiw00@bk.ru>
2019-04-26 13:00:15 +0300

    add zuul proxy

--- src/main/java/spring/boot/Front/FrontApplication.java
+++ src/main/java/spring/boot/Front/FrontApplication.java
@@ -1,14 +1,11 @@
 package spring.boot.Front;
 
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableZuulProxy
 public class FrontApplication {
    public static void main(String[] args) {
 		SpringApplication.run(FrontApplication.class, args);
